509c77eca6e7c7104063bc2f8ce797719a5bc6e9
James Richardson <me+github@james.richardson.name>
2012-07-29 17:07:06 +0100

    fix key mismatch

--- src/main/java/name/richardson/james/bukkit/timedmessages/management/ReloadCommand.java
+++ src/main/java/name/richardson/james/bukkit/timedmessages/management/ReloadCommand.java
@@ -62,7 +62,7 @@ public class ReloadCommand extends PluginCommand {
   private void registerPermissions() {
     final String prefix = this.plugin.getDescription().getName().toLowerCase() + ".";
     // create the base permission
-    final Permission base = new Permission(prefix + this.getName(), this.getMessage("reloadcommand-permission-description"), PermissionDefault.OP);
+    final Permission base = new Permission(prefix + this.getName(), this.getMessage("permission-description"), PermissionDefault.OP);
     base.addParent(this.plugin.getRootPermission(), true);
     this.addPermission(base);
   }
