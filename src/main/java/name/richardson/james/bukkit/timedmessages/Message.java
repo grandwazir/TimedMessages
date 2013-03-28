ea1829d8c7036c9f83f84512c7c0b4134022a44a
James Richardson <me+github@james.richardson.name>
2013-03-28 11:56:49 +0000

    reverse this check so if worlds is empty do not continue

--- src/main/java/name/richardson/james/bukkit/timedmessages/Message.java
+++ src/main/java/name/richardson/james/bukkit/timedmessages/Message.java
@@ -79,7 +79,7 @@ public abstract class Message implements Runnable {
  
     for (final Player player : this.server.getOnlinePlayers()) {
       // ignore the player if they are not in the world required
-      if (worlds.isEmpty() || !worlds.contains(player.getWorld().getName())) continue;    
+      if (!worlds.isEmpty() || !worlds.contains(player.getWorld().getName())) continue;    
       // if the player is not in the correct region ignore them
       if (!this.isPlayerInRegion(player)) continue;
       // ignore the player if they do not have the correct permission
